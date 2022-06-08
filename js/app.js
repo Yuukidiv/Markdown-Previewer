4351de44834ea04a9a0a6edd37ccf0263935026b
Yuukidiv <67429515+Yuukidiv@users.noreply.github.com>
2022-06-08 19:03:37 +0200

    Finished loaded text

--- js/app.js
+++ js/app.js
@@ -6,16 +6,21 @@ function syncro() {
     previewValue.innerHTML = markedValue;
 }
 function init () {
-    let defaultText = `# Header
+    let defaultText = `\
+# Header
 ## Sub Header 
 [Inline link](https://www.google.com)
-\`back-ticks around\`
-\`\`\`Code block () {
-console.log("Last day of school");}\`\`\`
+\`
+back-ticks around
+\`
+\`\`\`
+Code block () {
+console.log("Last day of school");}
+\`\`\`
 1. List Item
 > Blockquote
 **SupahBoldText**
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\`
+![freeCodeCamp Logo](https://www.icegif.com/wp-content/uploads/2021/11/icegif-1638.gif)\`
     `;
     let editorVal = document.getElementById("editor");
     editorVal.value = defaultText;
